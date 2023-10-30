fdb26e14e753083019130d6e5d976c04a37df485
paulocerez <paulo.ramirez@web.de>
2023-10-30 23:01:52 +0100

    init: scraping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const hello = await api.post.hello.query({
+    text: "my friend, nice to have you here.",
+  });
   const session = await getServerAuthSession();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#3747d7] to-[#131cc2] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-bold tracking-tight sm:text-[5rem]">
+        <h1 className="text-5xl font-medium tracking-tight sm:text-[5rem]">
           Matchdays.
         </h1>
         <div className="flex flex-col items-center gap-2">
@@ -25,7 +27,7 @@ export default async function Home() {
             </p>
             <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              className="rounded-lg bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
             >
               {session ? "Sign out" : "Sign in"}
             </Link>
@@ -42,12 +44,12 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+  const latestPost: string = (await api.post.getLatest.query()) as string;
 
   return (
     <div className="w-full max-w-xs">
       {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+        <p className="truncate">Your most recent post: {latestPost}</p>
       ) : (
         <p>You have no posts yet.</p>
       )}
