b5eb556e9d648168de59a7a5343514ceb9b20c41
paulocerez <paulo.ramirez@web.de>
2024-10-02 15:46:11 +0200

    fix: adjust scraper so that it does not need an extra convert function

--- src/utils/scraping/barcelonaScraper.ts
+++ src/utils/scraping/barcelonaScraper.ts
@@ -1,7 +1,7 @@
 import { InsertMatch } from "@/db/schema/teams";
 import axios from "axios";
 import * as cheerio from "cheerio";
-import convertDatetime from "../datetimeConverter";
+import dayjs from "dayjs";
 
 const params = {
   headers: {
@@ -13,7 +13,7 @@ const params = {
 export default async function scrapeMatchdayData(): Promise<InsertMatch[]> {
   const matches: InsertMatch[] = [];
   const response = await axios.get(
-    "https://onefootball.com/de/team/fc-barcelona-5/spiele",
+    "https://onefootball.com/de/team/fc-bayern-muenchen-6/spiele",
     params
   );
 
@@ -22,30 +22,37 @@ export default async function scrapeMatchdayData(): Promise<InsertMatch[]> {
 
   $("article.SimpleMatchCard_simpleMatchCard__yTuUP").each((_, element) => {
     const $element = $(element);
-    const datetimeString = $element.find("time").text().trim();
-    const datetimeRegex = /(\d{2}\.\d{2}\.\d{2})(\d{2}:\d{2})/;
-    const regexMatch = datetimeString.match(datetimeRegex);
+    const $timeElement = $element.find("time");
+    const datetimeAttribute = $timeElement.attr("datetime");
+    const competition = $element.find("footer p").text().trim();
+    const teams = $element
+      .find(".SimpleMatchCardTeam_simpleMatchCardTeam__name__7Ud8D")
+      .map((_, el) => $(el).text().trim())
+      .get()
+      .join(" : ");
 
-    if (regexMatch && regexMatch.length === 3) {
-      const date = regexMatch[1];
-      const time = regexMatch[2];
-      console.log(date, time);
-      const formattedDateTime = convertDatetime(date, time);
-      const competition = $element.find("footer p").text().trim();
-      const teams = $element
-        .find(".SimpleMatchCardTeam_simpleMatchCardTeam__name__7Ud8D")
-        .map((_, el) => $(el).text().trim())
-        .get()
-        .join(" : ");
+    if (datetimeAttribute) {
+      const dateObject = new Date(datetimeAttribute); // convert to Date
 
-      matches.push({
-        datetime: formattedDateTime,
-        match: teams,
-        competition,
-      });
+      // Check if the date is valid
+      if (!isNaN(dateObject.getTime())) {
+        matches.push({
+          datetime: dateObject,
+          match: teams,
+          competition,
+        });
+      } else {
+        console.warn(
+          "Invalid date format for match:",
+          teams,
+          datetimeAttribute
+        );
+      }
+    } else {
+      console.warn("No datetime attribute found for match:", teams);
     }
+    // }
   });
-  console.log(matches);
   return matches;
 }
 
